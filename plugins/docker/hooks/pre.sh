03836257e249624669166e286402116c881d5804
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2017-09-09 01:41:48 +0300

    fix: testing

--- plugins/docker/hooks/pre.sh
+++ plugins/docker/hooks/pre.sh
@@ -13,13 +13,16 @@ if ([[ "${taito_mode:-}" != "ci" ]] \
    echo "### docker - pre: Starting ###"
    echo
 
+   # TODO how to avoid running docker-in-docker on google container
+   # environment?
+   # https://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/
    file="${taito_project_path}/docker-compose.yaml"
    if [[ -f "${taito_project_path}/docker-test.yaml" ]]; then
      file="${taito_project_path}/docker-test.yaml"
    fi
 
    "${taito_cli_path}/util/execute-on-host.sh" \
-     "docker-compose up --build -f ${file}" && \
+     "docker-compose -f ${file} up" && \
 
    echo "Waiting for docker to start..." && \
    echo "TODO check status instead of hardcoded long wait." && \
