e507ede0e56568e0d40e1891c3a97d00c35e8694
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2017-09-28 18:08:56 +0300

    feat: o-start:local --prod

--- plugins/docker/util/start.sh
+++ plugins/docker/util/start.sh
@@ -4,12 +4,17 @@
 : "${taito_plugin_path:?}"
 : "${taito_command:?}"
 
-command="${1}"
+switches=" ${*} "
 
-if [[ "${command}" == "--clean" ]]; then
+setenv=""
+if [[ "${switches}" == *"--prod"* ]]; then
+  setenv="dockerfile=Dockerfile.build "
+fi
+
+if [[ "${switches}" == *"--clean"* ]]; then
   "${taito_plugin_path}/util/clean.sh" && \
   "${taito_cli_path}/util/execute-on-host-fg.sh" \
-    "docker-compose up --force-recreate --build"
+    "${setenv}docker-compose up --force-recreate --build"
 else
-  "${taito_cli_path}/util/execute-on-host-fg.sh" "docker-compose up"
+  "${taito_cli_path}/util/execute-on-host-fg.sh" "${setenv}docker-compose up"
 fi
