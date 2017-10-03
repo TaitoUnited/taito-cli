e85ace0be1cd4d7445702b6daadbd9272dc392f0
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2017-10-04 00:51:04 +0300

    feat: o-status --all

--- plugins/kubectl/o-status.sh
+++ plugins/kubectl/o-status.sh
@@ -4,7 +4,12 @@
 : "${taito_customer:?}"
 : "${taito_env:?}"
 
-switch="@{1}"
+switch="${1}"
+if [[ "${switch}" == "--all" ]]; then
+  params=("${@:2}")
+else
+  params=("${@:1}")
+fi
 
 echo
 echo "### kubectl - o-status: Showing status of ${taito_env} ###"
@@ -13,21 +18,22 @@ echo "### kubectl - o-status: Showing status of ${taito_env} ###"
 "${taito_plugin_path}/util/use-context.sh"
 
 if [[ "${switch}" == "--all" ]]; then
+  switch=
   echo
   echo "--- Nodes ---"
   kubectl describe nodes
 fi
 echo
 echo "--- Ingress ---"
-kubectl get ingress "${@}"
+kubectl get ingress "${params[@]}"
 echo
 echo
 echo "--- Services ---"
-kubectl get services "${@}"
+kubectl get services "${params[@]}"
 echo
 echo
 echo "--- Pods ---"
-kubectl get pods "${@}"
+kubectl get pods "${params[@]}"
 
 # Call next command on command chain
 "${taito_cli_path}/util/call-next.sh" "${@}"
