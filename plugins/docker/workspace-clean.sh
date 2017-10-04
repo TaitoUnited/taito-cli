6f925d4d4eb1c1e7291d627d4ddcb801f2d79f17
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2017-10-04 19:31:55 +0300

    feat: workspace-clean

--- plugins/docker/workspace-clean.sh
+++ plugins/docker/workspace-clean.sh
@@ -6,7 +6,7 @@ echo
 echo "### docker - workspace-clean: Cleaning old images ###"
 
 "${taito_cli_path}/util/execute-on-host-fg.sh" \
-  "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /etc:/etc:ro --entrypoint /docker-gc/docker-gc taito"
+  "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /etc:/etc:ro --entrypoint /docker-gc/docker-gc --rm ${taito_image_name}"
 
 # Call next command on command chain
 "${taito_cli_path}/util/call-next.sh" "${@}"
