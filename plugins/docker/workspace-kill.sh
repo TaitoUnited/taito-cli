7632a9b4e44f5f5f51b74aaab82ac7930dd17919
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2017-10-05 15:07:44 +0300

    fix: Fixed workspace-kill

--- plugins/docker/workspace-kill.sh
+++ plugins/docker/workspace-kill.sh
@@ -6,7 +6,7 @@ echo
 echo "### docker - workspace-kill: Killing all containers ###"
 
 "${taito_cli_path}/util/execute-on-host-fg.sh" \
-  "docker kill $(docker ps -q)" && \
+  "docker kill \$(docker ps -q)" && \
 
 # Call next command on command chain
 "${taito_cli_path}/util/call-next.sh" "${@}"
