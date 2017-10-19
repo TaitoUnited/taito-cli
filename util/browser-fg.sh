9666d3a3df67edaff3519e57216f78bdad62d64c
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2017-10-19 14:21:30 +0300

    fix(linux): Removed extra $ from link urls

--- util/browser-fg.sh
+++ util/browser-fg.sh
@@ -7,5 +7,5 @@ url="${1:?}"
 if [[ "${taito_host_uname}" == "Darwin" ]]; then
   ${taito_cli_path}/util/execute-on-host-fg.sh "open -a 'Google Chrome' '${url}'"
 else
-  ${taito_cli_path}/util/execute-on-host-fg.sh "xdg-open '${url}'$"
+  ${taito_cli_path}/util/execute-on-host-fg.sh "xdg-open '${url}'"
 fi
