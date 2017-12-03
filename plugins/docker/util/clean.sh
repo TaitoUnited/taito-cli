ae4a140a3f6b1924aed782940739a109e955904a
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2017-12-03 22:59:53 +0200

    chore: Output tuning.

--- plugins/docker/util/clean.sh
+++ plugins/docker/util/clean.sh
@@ -3,6 +3,7 @@
 : "${taito_cli_path:?}"
 
 # TODO [data | build] as arguments
+echo "Docker will remove images and volumes after taito-cli has exited"
 
 "${taito_cli_path}/util/execute-on-host-fg.sh" \
   "docker-compose down --rmi 'all' --volumes --remove-orphans"
--- plugins/npm/oper-clean.sh
+++ plugins/npm/oper-clean.sh
@@ -3,6 +3,7 @@
 : "${taito_cli_path:?}"
 : "${taito_project_path:?}"
 
+echo "Deleting all node_modules directories recursively"
 echo "NOTE: Remember to run 'taito install' after clean"
 
 find "${taito_project_path}" -name "node_modules" -type d -prune -exec \
