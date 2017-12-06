fa632a28f0176451112918bc735b8762ae8a57f1
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2017-12-07 00:52:57 +0200

    feat(git): Implemented git-env and added push origin confirmation to git-feat.

--- plugins/git/git-env.sh
+++ plugins/git/git-env.sh
@@ -2,10 +2,12 @@
 
 : "${taito_cli_path:?}"
 : "${taito_plugin_path:?}"
+: "${taito_env:?}"
 
-source="${taito_branch:-dev}"
-dest="feature/${1:?Feature name not given}"
+source="${1:-dev}"
+dest="${taito_branch:?Destination branch name not given}"
 
+# TODO duplicate code with git-feat.sh?
 "${taito_cli_path}/util/execute-on-host-fg.sh" "\
   if ! git checkout ${dest} 2> /dev/null; then \
     echo 'Creating a new branch: ${dest}. Do you want to continue (Y/n)?' && \
@@ -17,11 +19,7 @@ dest="feature/${1:?Feature name not given}"
     git checkout ${source} && \
     git pull && \
     git checkout -b ${dest} && \
-    echo 'Push the ${dest} branch to remote repository (y/N)?' && \
-    read -r confirm && \
-    if [[ \${confirm} =~ ^[Yy]$ ]]; then \
-      git push -u origin ${dest} \
-    fi && \
+    git push -u origin ${dest} \
   fi \
   " && \
 
