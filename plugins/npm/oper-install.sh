ece3e8d76d9d3d0a0ba48db5b8cafff33a8c42c9
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-02-27 11:36:17 +0200

    fix: taito install permission fix

--- plugins/npm/oper-install.sh
+++ plugins/npm/oper-install.sh
@@ -10,11 +10,18 @@ if [[ "${switches}" == *" --clean "* ]]; then
   # (${taito_setv:?}; su taito -s /bin/sh -c 'npm cache clean')
 fi && \
 echo "# Running 'npm install'..." && \
-(${taito_setv:?}; su taito -s /bin/sh -c 'npm install') && \
+# NOTE: possible postinstalls will fail because running npm install as root
+# inside taito container and npm will try to downgrade permissions before
+# running postinstall script. However we cannot run install as taito user
+# because then CI/CD will fail because of file permissions.
+# NOTE: postinstall will run ok as root if --unsafe-perm switch is set
+# but it must be passed on if postinstall calls npm install
+(${taito_setv:?}; npm install --unsafe-perm) && \
 
 if [[ "${switches}" == *" --all "* ]]; then
   echo
-  echo "# Running 'npm run install-all'" && \
+  echo "# Running 'npm run install-all'"
+  # postinstalls will fail if run as root
   (${taito_setv:?}; su taito -s /bin/sh -c 'npm run install-all')
 fi && \
 
