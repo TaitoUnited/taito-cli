02ca71f29d1de054c98c8ca363b8c9a53c675519
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-03-01 21:12:32 +0200

    feat: ci_exec_test_wait setting

--- plugins/basic/ci-wait.sh
+++ plugins/basic/ci-wait.sh
@@ -3,10 +3,9 @@
 : "${taito_app_url:?}"
 
 if [[ "${taito_mode:-}" != "ci" ]] || [[ "${ci_exec_test:-}" == "true" ]]; then
-  echo "Waiting for version change on ${taito_app_url}"
-
-  echo "TODO implement version check instead of sleep"
-  sleep 120
+  echo "Waiting ${ci_exec_test_wait:-120} seconds for version change on ${taito_app_url}"
+  echo "TODO implement version check instead of sleep?"
+  sleep "${ci_exec_test_wait:-120}"
 fi && \
 
 # Call next command on command chain
