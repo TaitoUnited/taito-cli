1f26a4e27cdbb23097f64469d8e0775f49dc3159
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-03-10 19:06:34 +0200

    feat: preliminary mysql support

--- plugins/mysql-db/db-open.sh
+++ plugins/mysql-db/db-open.sh
@@ -8,8 +8,15 @@
 
 username="${1}"
 
-echo "host: ${database_host} port:${database_port}"
-echo "TODO implement"
+# TODO determine username automatically if not given
+if [[ ! "${username}" ]]; then
+  username="${database_name}_app"
+fi
+
+echo "host:${database_host} port:${database_port} database:${database_name} username:${username}"
+
+# TODO determine password from secrets
+MYSQL_PWD="secret" mysql -u "${username}" -h "${database_host}" -P "${database_port}" -D "${database_name}"
 
 # Call next command on command chain
 "${taito_cli_path}/util/call-next.sh" "${@}"
