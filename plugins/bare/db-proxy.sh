5dfbc2f2934a8e45f54152fff6085f889f135141
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-03-11 03:13:14 +0200

    chore: bare plugin was replaced by ssh plugin

--- plugins/bare/db-proxy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-: "${taito_cli_path:?}"
-
-# TODO open db proxy connection to machine?
-
-echo "TODO implement" && \
-
-# Call next command on command chain
-"${taito_cli_path}/util/call-next.sh" "${@}"
