24c6a2e89ee7b6236e185bee3e191fdffbe6a43a
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-04-07 17:03:42 +0300

    feat: Removed ci prefix from npm release commands

--- plugins/semantic/ci-release-pre.sh
+++ plugins/semantic/ci-release-pre.sh
@@ -10,7 +10,7 @@ if [[ -z "${secret_value_git_github_build}" ]]; then
   secret_value_git_github_build="${secret_value_ext_github_build:-}"
 fi
 
-command=ci-release-pre:${taito_env}
+command=release-pre:${taito_env}
 
 # Run the command only if it exists in package.json
 commands=$(npm run | grep '^  [^ ]*$' | sed -e 's/ //g')
