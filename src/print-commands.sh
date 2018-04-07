3ffb007f46c8524310e2d94b1e5ad2429c70666b
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-04-07 22:12:36 +0300

    feat: Print more commands

--- src/print-commands.sh
+++ src/print-commands.sh
@@ -8,13 +8,18 @@ features=$(git branch -a | grep " feature/" | sed -e 's|feature/||')
 (
   # Basic commands
   echo "--help"
+  echo "--readme"
+  echo "--shell"
+  echo "--trouble"
   echo "--upgrade"
   echo "install"
   echo "install --clean"
+  echo "env"
+  echo "build"
   echo "db add: [NAME]"
   echo "template upgrade"
-  echo "workspace kill"
   echo "workspace clean"
+  echo "workspace kill"
 
   # Version control commands
   echo "vc env list"
@@ -36,6 +41,30 @@ features=$(git branch -a | grep " feature/" | sed -e 's|feature/||')
     echo "clean: ${stack}"
   done
 
+  # Project management
+  # NOTE: Advanced
+  echo "project apply"
+  echo "project destroy"
+  echo "project docs"
+  echo "project contacts"
+
+  # Zone management
+  # TODO: Show only for a zone
+  # echo "zone apply"
+  # echo "zone status"
+  # echo "zone doctor"
+  # echo "zone maintenance"
+  # echo "zone destroy"
+
+  # Passwords
+  echo "passwd share"
+  echo "passwd list"
+  echo "passwd list: [FILTER]"
+  echo "passwd get: [NAME]"
+  echo "passwd set: [NAME]"
+  echo "passwd rotate"
+  echo "passwd rotate: [FILTER]"
+
   # Environment specific commands
   envs="local ${taito_environments:-}"
   for env in ${envs}
@@ -47,25 +76,27 @@ features=$(git branch -a | grep " feature/" | sed -e 's|feature/||')
 
     echo "start${suffix}"
     echo "stop${suffix}"
+    # TODO run:ios run:android
     echo "init${suffix}"
     echo "init${suffix} --clean"
     echo "info${suffix}"
-    echo "status${suffix}"
-    echo "unit${suffix}"
     echo "test${suffix}"
-    echo "test${suffix} [SUITE]"
+    echo "secrets${suffix}"
+    echo "status${suffix}"
 
-    echo "db open${suffix}"
     echo "db proxy${suffix}"
+    echo "db open${suffix}"
     echo "db import${suffix} [FILE]"
     echo "db dump${suffix}"
     echo "db log${suffix}"
-    echo "db revert${suffix} [CHANGE]"
     echo "db recreate${suffix}"
     echo "db deploy${suffix}"
     echo "db rebase${suffix}"
+    echo "db rebase${suffix} [CHANGE]"
+    echo "db revert${suffix} [CHANGE]"
     echo "db diff${suffix} [SOURCE_ENV]"
     echo "db copy${suffix} [SOURCE_ENV]"
+    echo "db copyquick${suffix} [SOURCE_ENV]"
 
     # Local-only commands
     if [[ "${env}" == "local" ]]; then
@@ -73,6 +104,11 @@ features=$(git branch -a | grep " feature/" | sed -e 's|feature/||')
       echo "start${suffix} --background --clean"
       echo "start${suffix} --clean"
       echo "start${suffix} --prod --clean"
+      echo "unit${suffix}"
+      echo "analyze${suffix}"
+      echo "scan${suffix}"
+      echo "docs${suffix}"
+      echo "lock${suffix}"
     fi
 
     # Remote-only commands
@@ -80,9 +116,13 @@ features=$(git branch -a | grep " feature/" | sed -e 's|feature/||')
       echo "--auth${suffix}"
       echo "vc env: ${env}"
 
+      # NOTE: Advanced
       echo "env apply${suffix}"
       echo "env rotate${suffix}"
       echo "env destroy${suffix}"
+      # echo "env alt apply${suffix}"
+      # echo "env alt rotate${suffix}"
+      # echo "env alt destroy${suffix}"
 
       echo "depl deploy${suffix} [IMAGE_TAG]"
       echo "depl cancel${suffix}"
@@ -93,8 +133,13 @@ features=$(git branch -a | grep " feature/" | sed -e 's|feature/||')
     # Stack component commands
     for stack in ${ci_stack}
     do
-      echo "shell${suffix} ${stack}"
+      echo "test${suffix} ${stack}"
+      echo "test${suffix} ${stack} [SUITE]"
       echo "logs${suffix} ${stack}"
+      echo "shell${suffix} ${stack}"
+      echo "exec${suffix} ${stack} - [COMMAND]"
+      echo "cp${suffix} ${stack}:[PATH] [PATH]"
+      echo "cp${suffix} [PATH] ${stack}:[PATH]"
       echo "kill${suffix} ${stack}"
       echo "depl build${suffix} ${stack}"
     done
