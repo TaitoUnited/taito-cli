a1c7ff319954b11fff753575875a896b9db3cbff
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-04-21 14:40:18 +0300

    chore(test-util): tuning

--- plugins/basic/util-test.sh
+++ plugins/basic/util-test.sh
@@ -22,19 +22,21 @@ if [[ "${taito_mode:-}" == "ci" ]]; then
   docker images
 fi
 
-# shellcheck disable=SC1090
-. "${taito_cli_path}/plugins/docker/util/determine-pod.sh" "${dir}" && \
-
 # Determine command to be run on init phase
 init_command="echo 'Not running init (ci_exec_test_init=false)'" && \
 if [[ "${ci_exec_test_init:-}" == "true" ]]; then
   init_command="taito init:${taito_env} --clean"
 fi && \
 
+# Determine test suite parameters
 # Pass all environment variables with the "test_${dir}_" prefix for the test.
 # Creates docker parameters: -e ENV_VAR='value' -e ENV_VAR2='value2' ...
 docker_env_vars=$(env | grep "test_${dir}_" | sed "s/^test_${dir}_/-e /" \
   | sed "s/=/='/" | sed "s/$/'/" | tr '\n' ' ' | sed 's/.$//') && \
+
+# Determine pod
+# shellcheck disable=SC1090
+. "${taito_cli_path}/plugins/docker/util/determine-pod.sh" "${dir}" && \
 
 # Determine command to be run on test phase
 compose_pre_cmd=""
@@ -58,7 +60,9 @@ template="echo 'SUITE START' && ${init_command} && ${compose_cmd} && echo 'SUITE
 
 # Generate commands to be run by traversing all test suites
 commands="" && \
-suites=($(cat ./${dir}/test-suites | grep "${suite_filter}")) && \
+echo "Reading test suites from ./${dir}/test-suites" && \
+suites=($(cat "./${dir}/test-suites" >2 /dev/null | \
+  grep "${suite_filter}")) && \
 for suite in "${suites[@]}"
 do
   commands="${commands} && ${template//SUITE/$suite}"
