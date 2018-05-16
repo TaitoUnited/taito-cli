7ce0543a678191d845af9f17fe9ef1854fe54b9c
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-05-16 11:31:20 +0300

    feat: 'taito commit: [CONTAINER]'

---
+++ plugins/docker/oper-commit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+: "${taito_cli_path:?}"
+: "${taito_plugin_path:?}"
+
+: "${1:?Container not given}"
+
+# shellcheck disable=SC1090
+. "${taito_plugin_path}/util/determine-pod.sh" "${@}"
+
+echo container: ${pod:?}
+
+"${taito_cli_path}/util/execute-on-host.sh" \
+  "docker ps; docker commit ${pod:?} ${pod:?}-savetus; echo ------"
+"${taito_cli_path}/util/execute-on-host-fg.sh" \
+  "docker image tag ${pod:?}-savetus ${pod:?}"
+
+# Call next command on command chain
+"${taito_cli_path}/util/call-next.sh" "${@}"
