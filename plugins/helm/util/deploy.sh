c16dffb31d91d740df6310784152116e98695b91
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-05-26 11:28:04 +0300

    feat(helm): helm_charts environment variable

--- plugins/helm/util/deploy.sh
+++ plugins/helm/util/deploy.sh
@@ -31,47 +31,67 @@ if [[ -z ${image} ]]; then
   exit 1
 fi
 
-# Execute
+# Deploy chart located in ./scripts/helm
 
-# helm-ENV.yaml overrides default settings of helm.yaml
-override_file=""
-if [[ -f "scripts/helm-${taito_env}.yaml" ]]; then
-  override_file="-f scripts/helm-${taito_env}.yaml"
-fi
+if [[ -d "./scripts/helm" ]]; then
+  # helm-ENV.yaml overrides default settings of helm.yaml
+  override_file=""
+  if [[ -f "scripts/helm-${taito_env}.yaml" ]]; then
+    override_file="-f scripts/helm-${taito_env}.yaml"
+  fi
 
-# Determine helm chart path
-if [[ -n "${helm_chart:-}" ]]; then
-  chart_path="${helm_chart}"
-elif [[ -d "./scripts/${taito_project}" ]]; then
-  chart_path="./scripts/${taito_project}"
-else
-  chart_path="./scripts/helm"
-fi
+  echo "- Deploying ${image} of ${taito_project}-${taito_env} using Helm"
+  (
+    ${taito_setv:?}
+    helm upgrade "${options[@]}" --debug --install \
+      --namespace "${taito_namespace}" \
+      --set env="${taito_env}" \
+      --set zone.name="${taito_zone}" \
+      --set zone.provider="${taito_provider:-}" \
+      --set project.name="${taito_project}" \
+      --set project.company="${taito_company:-}" \
+      --set project.family="${taito_family:-}" \
+      --set project.application="${taito_application:-}" \
+      --set project.suffix="${taito_suffix:-}" \
+      --set build.imageTag="${image}" \
+      --set build.version="${version}" \
+      --set build.commit="TODO" \
+      -f scripts/helm.yaml ${override_file} \
+      ${helm_deploy_options:-} \
+      "${taito_project}-${taito_env}" "./scripts/helm"
+  )
+fi && \
 
-# Set version
-version_option=""
-if [[ -n "${helm_chart_version:-}" ]]; then
-  version_option="--version ${helm_chart_version}"
-fi
+# Deploy charts defined in helm_charts environment variable
 
-echo "- Deploying ${image} of ${taito_project}-${taito_env} using Helm"
+charts=("${helm_charts}")
+for chart in ${charts[@]}
+do
+  echo "- Deploying chart ${chart} for ${taito_project}-${taito_env} using Helm"
+  chart_name="${chart##*/}"
 
-(
-  ${taito_setv:?}
-  helm upgrade "${options[@]}" --debug --install \
-    --namespace "${taito_namespace}" \
-    --set env="${taito_env}" \
-    --set zone.name="${taito_zone}" \
-    --set zone.provider="${taito_provider:-}" \
-    --set project.name="${taito_project}" \
-    --set project.company="${taito_company:-}" \
-    --set project.family="${taito_family:-}" \
-    --set project.application="${taito_application:-}" \
-    --set project.suffix="${taito_suffix:-}" \
-    --set build.imageTag="${image}" \
-    --set build.version="${version}" \
-    --set build.commit="TODO" \
-    -f scripts/helm.yaml ${override_file} \
-    ${version_option} \
-    "${taito_project}-${taito_env}" "${chart_path}"
-)
+  # helm-ENV.yaml overrides default settings of helm.yaml
+  chart_override_file=""
+  if [[ -f "scripts/${chart_name}/helm-${taito_env}.yaml" ]]; then
+    chart_override_file="-f scripts/${chart_name}/helm-${taito_env}.yaml"
+  fi
+  (
+    ${taito_setv:?}
+    helm upgrade "${options[@]}" --debug --install \
+      --namespace "${taito_namespace}" \
+      --set env="${taito_env}" \
+      --set zone.name="${taito_zone}" \
+      --set zone.provider="${taito_provider:-}" \
+      --set project.name="${taito_project}" \
+      --set project.company="${taito_company:-}" \
+      --set project.family="${taito_family:-}" \
+      --set project.application="${taito_application:-}" \
+      --set project.suffix="${taito_suffix:-}" \
+      --set build.imageTag="${image}" \
+      --set build.version="${version}" \
+      --set build.commit="TODO" \
+      -f scripts/${chart_name}/helm.yaml ${chart_override_file} \
+      ${helm_deploy_options:-} \
+      "${chart_name}" "${chart}"
+  )
+done
