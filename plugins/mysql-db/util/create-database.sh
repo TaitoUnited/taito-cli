d4bafd281ba14e01b0f09d89a0d1f5abb48efd2b
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-06-03 13:44:34 +0300

    feat: Renamed init.sql to db.sql

--- plugins/mysql-db/util/create-database.sh
+++ plugins/mysql-db/util/create-database.sh
@@ -20,14 +20,18 @@
       -e "set @database='${database_name}'; set @dbusermgr='${database_name}'; set @dbuserapp='${database_name}ap'; source ${taito_plugin_path}/resources/create.sql ;"
   ) && \
 
-  echo
-  echo "- import ./${taito_target:-database}/init.sql"
-  (
-    ${taito_setv:?}
-    mysql -h "${database_host}" \
-    -P "${database_port}" \
-    -D "${database_name}" \
-    -u "${database_username}" \
-    -e "source ./${taito_target:-database}/init.sql ;"
-  )
+  if [[ -f "./${taito_target:-database}/db.sql" ]]; then
+    echo
+    echo "- import ./${taito_target:-database}/db.sql"
+    (
+      ${taito_setv:?}
+      mysql -h "${database_host}" \
+      -P "${database_port}" \
+      -D "${database_name}" \
+      -u "${database_username}" \
+      -e "source ./${taito_target:-database}/db.sql ;"
+    )
+  else
+    echo "WARN: File ./${taito_target:-database}/db.sql does not exist"
+  fi
 )
--- plugins/postgres-db/util/create-database.sh
+++ plugins/postgres-db/util/create-database.sh
@@ -12,27 +12,44 @@
   echo
   echo "- import create.sql"
   PGPASSWORD="${PGPASSWORD}"
-  (${taito_setv:?}; psql -h "${database_host}" \
-    -p "${database_port}" -U "${database_username}" \
-    -f "${taito_plugin_path}/resources/create.sql" \
-    -v "database=${database_name}" \
-    -v "dbuserapp=${database_name}_app") && \
+  (
+    ${taito_setv:?}
+    psql -h "${database_host}" \
+      -p "${database_port}"
+      -U "${database_username}" \
+      -f "${taito_plugin_path}/resources/create.sql" \
+      -v "database=${database_name}" \
+      -v "dbuserapp=${database_name}_app"
+  ) && \
 
-  echo
-  echo "- import ./${taito_target:-database}/init.sql"
-  (${taito_setv:?}; psql -h "${database_host}" \
-    -p "${database_port}" -d "${database_name}" \
-     -U "${database_username}" < "./${taito_target:-database}/init.sql") && \
+  if [[ -f "./${taito_target:-database}/db.sql" ]]; then
+    echo
+    echo "- import ./${taito_target:-database}/db.sql"
+    (
+      ${taito_setv:?}
+      psql -h "${database_host}" \
+        -p "${database_port}"
+        -d "${database_name}" \
+        -U "${database_username}" \
+        < "./${taito_target:-database}/db.sql"
+    )
+  else
+    echo "WARN: File ./${taito_target:-database}/db.sql does not exist"
+  fi && \
 
   . "${taito_plugin_path}/util/postgres-username-password.sh" && \
 
   echo && \
   echo "- import grant.sql" && \
   PGPASSWORD="${secret_value}"
-  (${taito_setv:?}; psql -h "${database_host}" -p "${database_port}" \
+  (
+    ${taito_setv:?}
+    psql -h "${database_host}"
+    -p "${database_port}" \
     -d "${database_name}" \
     -U "${database_name}" \
     -f "${taito_plugin_path}/resources/grant.sql" \
     -v "database=${database_name}" \
-    -v "dbuserapp=${database_name}_app")
+    -v "dbuserapp=${database_name}_app"
+  )
 )
