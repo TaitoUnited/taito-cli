d20d6d3e6624dff0b4ed9631dfc15459956fd040
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-06-06 23:40:47 +0300

    feat(ci-build): Wait for image if it does not exist yet

--- plugins/docker/ci-build.sh
+++ plugins/docker/ci-build.sh
@@ -30,51 +30,66 @@ version=$(grep "version" "${taito_project_path}/package.json" | \
 if [[ "${taito_targets:-}" != *"${name}"* ]]; then
   echo "Skipping build: ${name} not included in taito_targets"
 else
-  if [[ ! -f ./taitoflag_images_exist ]]; then
-    if [[ "${taito_mode:-}" == "ci" ]] && [[ "${ci_exec_build:-}" == "false" ]]; then
-      echo "- ERROR: Image does not exist and not building a new one because ci_exec_build is false"
-      exit 1
-    else
-      echo "- Building image"
+  if [[ -f ./taitoflag_images_exist ]] || \
+     ([[ "${taito_mode:-}" == "ci" ]] && [[ "${ci_exec_build:-}" == "false" ]])
+  then
+    # Image should exist.
+    # We have to pull the existing image so that it exists at the end
+    # We also pull the builder image for integration tests
+    count=0
+    pulled="false"
+    while true
+    do
+      echo "- Pulling the existing image ${image_tag}."
       (
-        ${taito_setv:?}
-        # Pull latest builder and production image to be used as cache
-        docker pull "${image_builder}"
-        docker pull "${image_latest}"
-        # Build the build stage container separately so that it can be used as:
-        # 1) Build cache for later builds using --cache-from
-        # 2) Integration and e2e test executioner
-        docker build \
-          --target builder \
-          -f "./${name}/Dockerfile.build" \
-          --build-arg BUILD_VERSION="${version}" \
-          --build-arg BUILD_IMAGE_TAG="${image_tag}" \
-          --cache-from "${image_builder}" \
-          --tag "${image_builder}" \
-          --tag "${image_tester}" \
-          "./${name}" && \
-        # Build the production runtime
-        # TODO use also latest production container as cache?
-        docker build \
-          -f "./${name}/Dockerfile.build" \
-          --cache-from "${image_builder}" \
-          --cache-from "${image_latest}" \
-          --build-arg BUILD_VERSION="${version}" \
-          --build-arg BUILD_IMAGE_TAG="${image_tag}" \
-          --tag "${image}" \
-          --tag "${image_latest}" \
-          "./${name}"
-      )
-    fi
+        ${taito_setv:?};
+        docker pull "${image}" && \
+        docker pull "${image_builder}" && \
+        docker image tag "${image_builder}" "${image_tester}"
+      ) && pulled="true"
+      if [[ $pulled == "true" ]]; then
+        break
+      elif [[ $count -gt 20 ]]; then
+        echo "- ERROR: Image ${image_tag} not found event after multiple retries and not building a new one because ci_exec_build is false"
+        exit 1
+      else
+        echo "- WARN: Image ${image_tag} not found and not building a new one because ci_exec_build is false. Retry in 30 secs."
+        sleep 30
+      fi
+    done
   else
-    echo "- Image ${image_tag} already exists. Pulling the existing image."
-    # We have pull the image so that it exists at the end
-    (${taito_setv:?}; docker pull "${image}")
-    # We have to also pull the builder image for integration tests
-    (${taito_setv:?}; docker pull "${image_builder}")
-    (${taito_setv:?}; docker image tag "${image_builder}" "${image_tester}")
+    # Image does not exist. Build it.
+    echo "- Building image"
+    (
+      ${taito_setv:?}
+      # Pull latest builder and production image to be used as cache
+      docker pull "${image_builder}"
+      docker pull "${image_latest}"
+      # Build the build stage container separately so that it can be used as:
+      # 1) Build cache for later builds using --cache-from
+      # 2) Integration and e2e test executioner
+      docker build \
+        --target builder \
+        -f "./${name}/Dockerfile.build" \
+        --build-arg BUILD_VERSION="${version}" \
+        --build-arg BUILD_IMAGE_TAG="${image_tag}" \
+        --cache-from "${image_builder}" \
+        --tag "${image_builder}" \
+        --tag "${image_tester}" \
+        "./${name}" && \
+      # Build the production runtime
+      # TODO use also latest production container as cache?
+      docker build \
+        -f "./${name}/Dockerfile.build" \
+        --cache-from "${image_builder}" \
+        --cache-from "${image_latest}" \
+        --build-arg BUILD_VERSION="${version}" \
+        --build-arg BUILD_IMAGE_TAG="${image_tag}" \
+        --tag "${image}" \
+        --tag "${image_latest}" \
+        "./${name}"
+    )
   fi && \
-
   if [[ "${taito_mode:-}" == "ci" ]]; then
     echo "Docker images after build:" && \
     docker images
