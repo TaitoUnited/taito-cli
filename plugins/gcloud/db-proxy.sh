16363b5a91f1c98193ca66bbe9d87f5f99862b4c
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-06-10 21:41:57 +0300

    feat: db proxy port as argument

--- plugins/gcloud/db-proxy.sh
+++ plugins/gcloud/db-proxy.sh
@@ -6,15 +6,24 @@
 : "${gcloud_zone:?}"
 : "${database_name:?}"
 
-echo "Database connection details:"
-echo "- host: 127.0.0.1"
-echo "- port: ${database_port:-}"
-echo "- database: ${database_name:-}"
-echo "- username: ${database_username:-}, ${database_name}, ${database_name}_app, ${database_name}ap or your personal username"
-echo "- password: ${database_password:-?}"
+port="${1}"
 
-"${taito_plugin_path}/util/db-proxy-start.sh" && \
-"${taito_plugin_path}/util/db-proxy-stop.sh" && \
+(
+  if [[ "${port}" ]]; then
+    export database_port="${port}"
+    export database_proxy_port="${port}"
+  fi
+
+  echo "Database connection details:"
+  echo "- host: 127.0.0.1"
+  echo "- port: ${database_port:-}"
+  echo "- database: ${database_name:-}"
+  echo "- username: ${database_username:-}, ${database_name}, ${database_name}_app, ${database_name}ap or your personal username"
+  echo "- password: ${database_password:-?}"
+
+  "${taito_plugin_path}/util/db-proxy-start.sh" && \
+  "${taito_plugin_path}/util/db-proxy-stop.sh"
+) && \
 
 # Call next command on command chain
 "${taito_cli_path}/util/call-next.sh" "${@}"
