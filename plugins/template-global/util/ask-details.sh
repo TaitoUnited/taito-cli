823093c972af0c4f74656103c2f94c4a55d49726
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-06-11 22:14:12 +0300

    feat: Do not allow special characters in names

--- plugins/template-global/util/ask-details.sh
+++ plugins/template-global/util/ask-details.sh
@@ -15,38 +15,45 @@ export taito_repo_name
 
 echo
 echo "Repository name, namespace and labels will be constituted from the following"
-echo "details. Please give a short word or abbreviation for each:"
+echo "details:"
 echo
 echo "  1) Company"
 echo "  2) Product family (optional)"
 echo "  3) Application name"
 echo "  4) Implementation suffix (optional)"
 echo
+echo "NOTE: Please give a short word or abbreviation for each."
+echo "No special characters, only alphabets!"
+echo
 echo "1) Company (e.g. 'seedi')?"
 read -r taito_company
-if [[ -z "${taito_company}" ]] || [[ ${#taito_company} -gt 12 ]]; then
-  echo "ERROR: not given or too long"
+if ! [[ "${taito_company}" =~ ^[a-zA-Z]+$ ]] || \
+   [[ ${#taito_company} -gt 14 ]]; then
+  echo "ERROR: invalid value or too long"
   exit 1
 fi
 echo
 echo "2) Optional: Product family (e.g. 'merri')?"
 read -r taito_family
-if [[ ${#taito_family} -gt 14 ]]; then
-  echo "ERROR: too long"
+if ! [[ "${taito_family}" =~ ^[a-zA-Z]*$ ]] || \
+   [[ ${#taito_family} -gt 14 ]]; then
+  echo "ERROR: invalid value or too long"
   exit 1
 fi
 echo
 echo "3) Application name (e.g. 'chat')?"
-read -r taito_application # TODO application
-if [[ -z "${taito_application}" ]] || [[ ${#taito_application} -gt 14 ]]; then
-  echo "ERROR: not given or too long"
+read -r taito_application
+if ! [[ "${taito_application}" =~ ^[a-zA-Z]+$ ]] || \
+   [[ ${#taito_application} -gt 14 ]]; then
+  echo "ERROR: invalid value or too long"
   exit 1
 fi
 echo
 echo "4) Optional: implementation suffix (e.g. 'api', 'gui', ...)"
 read -r taito_suffix # TODO application_suffix
-if [[ ${#taito_suffix} -gt 10 ]]; then
-  echo "ERROR: too long"
+if ! [[ "${taito_suffix}" =~ ^[a-zA-Z]*$ ]] || \
+   [[ ${#taito_suffix} -gt 10 ]]; then
+  echo "ERROR: invalid value or too long"
   exit 1
 fi
 
