30475094c6e9e03fa36ed9e913a2bb0cbbc2239f
Nico Nysten <nico.casper@hotmail.com>
2018-07-19 16:22:18 +0300

    fix: remove taito_env and taito_project from pre hook for now
    Didn' t work

--- plugins/basic/hooks/pre.sh
+++ plugins/basic/hooks/pre.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 : "${taito_cli_path:?}"
 : "${taito_plugin_path:?}"
-: "${taito_env:?}"
-: "${taito_project:?}"
 
 # --auth command pre-handling
 if [[ "${taito_command:-}" == "__auth" ]]; then
