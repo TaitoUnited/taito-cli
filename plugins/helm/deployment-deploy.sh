8ad0668d1ade0bb1a741eea0c0835802f4dc8ad9
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-09-08 23:03:20 +0300

    feat: Implemented 'taito deployment deploy:ENV'

--- plugins/helm/deployment-deploy.sh
+++ plugins/helm/deployment-deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 : "${taito_cli_path:?}"
+: "${taito_plugin_path:?}"
 
 "${taito_cli_path}/plugins/kubectl/util/use-context.sh" && \
-echo "TODO implement: confirm and then run 'ci deploy'"
+"${taito_plugin_path}/util/deploy.sh" "${@}"
 
 # Call next command on command chain
 "${taito_cli_path}/util/call-next.sh" "${@}"
