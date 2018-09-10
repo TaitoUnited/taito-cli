dd065c28b675514547f5ee9da638ca3bd0fa67f8
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-09-10 19:48:06 +0300

    feat: Run taito commands with taito user
    Issues #34, #36

--- src/taito-upgrade.sh
+++ src/taito-upgrade.sh
@@ -9,23 +9,39 @@
 # Pull taito-cli docker image
 docker pull "${taito_image}"
 
-# Copy credentials from old image
+# Prepare taito-new image for modificaions
 docker rm taito-save taito-new &> /dev/null
+docker create --name taito-new "${taito_image}"
+
+# Copy credentials from old image
 if docker create --name taito-save "${taito_image}save" &> /dev/null; then
-  docker create --name taito-new "${taito_image}"
   echo "Copying credentials from the old image."
-  mkdir -p ~/.taito/save &> /dev/null
+  mkdir -p ~/.taito/save/root &> /dev/null
+  mkdir -p ~/.taito/save/taito &> /dev/null
 
-  docker cp taito-save:/root/.config ~/.taito/save
-  docker cp taito-save:/root/.kube ~/.taito/save
-  docker cp taito-save:/root/admin_creds.enc ~/.taito/save &> /dev/null
-  docker cp ~/.taito/save/.config taito-new:/root
-  docker cp ~/.taito/save/.kube taito-new:/root
-  docker cp ~/.taito/save/admin_creds.enc taito-new:/root &> /dev/null
+  docker cp taito-save:/home/taito/.config ~/.taito/save/taito &> /dev/null
+  docker cp taito-save:/home/taito/.kube ~/.taito/save/taito &> /dev/null
+  docker cp taito-save:/root/.config ~/.taito/save/root &> /dev/null
+  docker cp taito-save:/root/.kube ~/.taito/save/root &> /dev/null
+  docker cp taito-save:/root/admin_creds.enc ~/.taito/save/root &> /dev/null
+  docker cp ~/.taito/save/taito/.config taito-new:/home/taito &> /dev/null
+  docker cp ~/.taito/save/taito/.kube taito-new:/home/taito &> /dev/null
+  docker cp ~/.taito/save/root/.config taito-new:/root &> /dev/null
+  docker cp ~/.taito/save/root/.kube taito-new:/root &> /dev/null
+  docker cp ~/.taito/save/root/admin_creds.enc taito-new:/root &> /dev/null
 
-  docker commit taito-new "${taito_image}" &> /dev/null
-  docker image tag "${taito_image}" "${taito_image}save"
   rm -rf ~/.taito/save
-  docker rm taito-save taito-new &> /dev/null
-  echo "DONE!"
 fi
+
+echo "Changing uid and gid to match the current user id"
+docker start taito-new &> /dev/null
+docker exec -it taito-new /bin/sh -c \
+  "groupadd --gid $(id -g) taitogroup 2> /dev/null || usermod --uid $(id -u) taito"
+sleep 3
+
+echo "Committing changes to taito image"
+docker commit taito-new "${taito_image}" &> /dev/null
+docker stop taito-new &> /dev/null
+docker image tag "${taito_image}" "${taito_image}save"
+docker rm taito-save taito-new &> /dev/null
+echo "DONE!"
