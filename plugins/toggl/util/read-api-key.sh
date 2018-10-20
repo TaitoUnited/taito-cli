5ea147039e0dea54f931d8023becdd5ebe806236
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-10-20 19:38:06 +0300

    fix(toggl): fixed api key check

--- plugins/toggl/util/read-api-key.sh
+++ plugins/toggl/util/read-api-key.sh
@@ -1,8 +1,9 @@
-#!/bin/bash -e
+#!/bin/bash
 
 export toggl_api_key
-toggl_api_key=$(cat ~/.toggl/api-key)
+toggl_api_key=$(cat ~/.toggl/api-key 2> /dev/null || :)
 
 if [[ ! "${toggl_api_key}" ]]; then
   echo "Toggl API KEY is missing. Authenticate with 'taito hours auth: toggl'"
+  exit 1
 fi
