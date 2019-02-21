6dcf30c15f04547e51684ae8987fbb9dfd4c91ad
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-02-21 17:28:27 +0200

    feat: stop --down

--- plugins/docker-compose/stop.sh
+++ plugins/docker-compose/stop.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 : "${taito_util_path:?}"
 
-"${taito_util_path}/execute-on-host-fg.sh" "docker-compose stop" && \
+options=" ${*} "
+
+command="stop"
+if [[ "${options}" == *"--down"* ]]; then
+  command="down -v"
+fi
+
+"${taito_util_path}/execute-on-host-fg.sh" "docker-compose ${command}" && \
 
 # Call next command on command chain
 "${taito_util_path}/call-next.sh" "${@}"
