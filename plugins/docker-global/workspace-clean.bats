aa17d9f9675224b8bfe933614523b01b7aefe3a4
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-03-08 18:16:58 +0200

    feat(workspace-clean): clean <none> images

--- plugins/docker-global/workspace-clean.bats
+++ plugins/docker-global/workspace-clean.bats
@@ -8,6 +8,7 @@ load "${taito_cli_path}/test/util/test-helper.sh"
   assert_executed docker stop CONTAINER_ID_1 CONTAINER_ID_2 CONTAINER_ID_3
   assert_executed docker volume prune
   assert_executed docker system prune -a --filter label!=fi.taitounited.taito-cli
+  assert_executed docker rmi
   assert_executed call-next.sh
-  assert_executed_count 4
+  assert_executed_count 5
 }
--- plugins/docker-global/workspace-clean.sh
+++ plugins/docker-global/workspace-clean.sh
@@ -10,7 +10,8 @@ echo docker volume prune: ; \
 docker volume prune; \
 echo ; \
 echo docker system prune: ; \
-docker system prune -a --filter 'label!=fi.taitounited.taito-cli'"
+docker system prune -a --filter 'label!=fi.taitounited.taito-cli' ; \
+docker rmi $(docker images | grep "<none>" | tr -s ' ' | cut -d ' ' -f 3)"
 
 # Call next command on command chain
 "${taito_util_path}/call-next.sh" "${@}"
