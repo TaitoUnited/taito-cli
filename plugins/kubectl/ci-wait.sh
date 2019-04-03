70b813854eb28e77070f404b9d8219be7dd746d3
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-04-03 23:08:31 +0300

    feat: ci wait timing improvement

--- plugins/kubectl/ci-wait.sh
+++ plugins/kubectl/ci-wait.sh
@@ -5,12 +5,13 @@
 "${taito_plugin_path}/util/use-context.sh"
 
 # Wait until all containers have been deployed
-sleep 15
+sleep 5
 while [[ -n $(kubectl get pods | grep ContainerCreating) ]]
 do
   echo "Waiting until all containers have been deployed"
   sleep 5
 done
+sleep 5
 
 # Call next command on command chain
 "${taito_cli_path}/util/call-next.sh" "${@}"
