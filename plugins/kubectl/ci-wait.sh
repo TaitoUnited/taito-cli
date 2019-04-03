95d60a4b0e389cb25317ebac0f7beea401e1641f
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-04-03 22:01:12 +0300

    feat(ci-wait): wait until all containers have been deployed

---
+++ plugins/kubectl/ci-wait.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+: "${taito_cli_path:?}"
+: "${taito_plugin_path:?}"
+
+"${taito_plugin_path}/util/use-context.sh"
+
+# Wait until all containers have been deployed
+sleep 5
+while [[ -n $(kubectl get pods | grep ContainerCreating) ]]
+do
+  echo "Waiting until all containers have been deployed"
+  sleep 5
+done
+
+# Call next command on command chain
+"${taito_cli_path}/util/call-next.sh" "${@}"
