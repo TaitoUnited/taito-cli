9bfb61275ee8485ca2a8fc7d7ab211adb04c6e98
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-04-05 13:32:43 +0300

    chore(storage): project member rights

--- examples/zones/gcloud/terraform/storage.tf
+++ examples/zones/gcloud/terraform/storage.tf
@@ -12,3 +12,16 @@ resource "google_storage_bucket" "functions" {
   }
   prevent_destroy = true
 }
+
+resource "google_storage_bucket_acl" "functions" {
+  depends_on = [
+    "google_storage_bucket.functions"
+  ]
+  bucket = "${var.taito_zone_functions_bucket}"
+
+  role_entity = [
+    "OWNER:project-owners-${data.google_project.project.number}",
+    "OWNER:project-editors-${data.google_project.project.number}",
+    "READER:project-viewers-${data.google_project.project.number}"
+  ]
+}
