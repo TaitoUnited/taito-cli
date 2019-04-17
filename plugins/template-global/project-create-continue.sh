f3a884b6bcc39e4f218b255b05986b8dd241e7bb
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-04-17 09:02:01 +0300

    feat(project-create): wait until git repo exists

--- plugins/template-global/project-create-continue.sh
+++ plugins/template-global/project-create-continue.sh
@@ -31,23 +31,30 @@ if [[ -f "CONFIGURATION.md" ]]; then
 fi
 
 echo "Please wait..." && \
-"${taito_cli_path}/util/execute-on-host-fg.sh" "\
-  export GIT_PAGER='' && \
-  git init -q && \
-  git add . && \
-  git commit -q -m 'First commit' && \
-  git remote add origin ${template_dest_git}/${taito_vc_repository}.git && \
-  git push -q -u origin master &> /dev/null && \
-  git tag v0.0.0 && \
-  git push -q origin v0.0.0 && \
-  git checkout -q -b dev && \
-  git push -q -u origin dev &> /dev/null && \
-  echo && \
-  echo DONE! Now configure your project! && \
-  echo Press enter to open configuration instructions. && \
-  read -r && \
-  echo && \
-  taito -c util-browser https://github.com/${template_default_organization}/${taito_vc_repository}/blob/dev/${doc}"
+"${taito_cli_path}/util/execute-on-host-fg.sh" "
+  set -e
+  export GIT_PAGER=''
+  git init -q
+  git add .
+  git commit -q -m 'First commit'
+  git remote add origin ${template_dest_git}/${taito_vc_repository}.git
+  while ! git push -q -u origin master &> /dev/null
+  do
+    echo Failed pushing to ${template_dest_git}/${taito_vc_repository}.
+    echo Make sure the repository exists and press enter.
+    read -r
+  done
+  git tag v0.0.0
+  git push -q origin v0.0.0
+  git checkout -q -b dev
+  git push -q -u origin dev &> /dev/null
+  echo
+  echo DONE! Now configure your project!
+  echo Press enter to open configuration instructions.
+  read -r
+  echo
+  taito -c util-browser https://github.com/${template_default_organization}/${taito_vc_repository}/blob/dev/${doc}
+"
 
 # Call next command on command chain
 "${taito_cli_path}/util/call-next.sh" "${@}"
