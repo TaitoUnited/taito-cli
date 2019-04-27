db9fe2b918cdcc5fa7776f18ea65ff14d35bfaa8
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-04-27 23:12:32 +0300

    fix: aws kubernetes auth fixed

--- plugins/aws/util/get-credentials-kube.sh
+++ plugins/aws/util/get-credentials-kube.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-: "${kubernetes_cluster:?}"
-: "${kubernetes_user:?}"
+: "${kubernetes_name:?}"
 : "${aws_region:?}"
 
 profile=${aws_user_profile:-default}
 
 ${taito_setv:?}
 aws --profile "${profile}" eks --region "${aws_region}" update-kubeconfig \
-  --name "${kubernetes_cluster}" \
-  --alias "${kubernetes_user}" &> ${taito_vout:-}
+  --name "${kubernetes_name}" \
+  --alias "${kubernetes_name}" &> ${taito_vout:-}
