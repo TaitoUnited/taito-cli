830c3181b14c9843cca7e20497523442ab0043ec
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-05-05 17:05:49 +0300

    feat: avoid passing password via CLI for AWS

--- plugins/aws/util/get-credentials-ecr.sh
+++ plugins/aws/util/get-credentials-ecr.sh
@@ -4,10 +4,7 @@
 
 . "${taito_cli_path}/plugins/aws/util/aws-options.sh"
 
-# TODO: displays 'WARNING! Using --password via the CLI is insecure. Use --password-stdin.'
-# see -> https://github.com/aws/aws-cli/issues/2875
-
+# NOTE: one-liner login to avoid passing password via CLI:
+# https://github.com/aws/aws-cli/issues/2875#issuecomment-487244855
 ${taito_setv:?}
-docker_login_command=$(aws $aws_options ecr get-login --no-include-email --region "${taito_provider_region}")
-
-eval $docker_login_command
+aws $aws_options ecr get-login --no-include-email --region "${taito_provider_region}" | awk '{print $6}' | docker login -u AWS --password-stdin $(aws $aws_options ecr get-login --no-include-email --region "${taito_provider_region}" | awk '{print $7}')
