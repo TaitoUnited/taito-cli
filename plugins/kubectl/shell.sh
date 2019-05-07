d9e2c08217d47c8302c6fbfb9e0f8a230d1b5fb9
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-05-07 13:59:08 +0300

    fix: try running shell with and without container name

--- plugins/kubectl/shell.sh
+++ plugins/kubectl/shell.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 : "${taito_cli_path:?}"
 : "${taito_plugin_path:?}"
 : "${taito_namespace:?}"
 : "${taito_project:?}"
 
-"${taito_plugin_path}/util/exec.sh" "/bin/sh" && \
+"${taito_plugin_path}/util/use-context.sh"
+
+. "${taito_plugin_path}/util/determine-pod-container.sh"
+(${taito_setv:?}; kubectl exec -it "${pod}" -c "${container}" -- "/bin/sh") || \
+(${taito_setv:?}; kubectl exec -it "${pod}" -- "/bin/sh")
 
 # Call next command on command chain
 "${taito_cli_path}/util/call-next.sh" "${@}"
