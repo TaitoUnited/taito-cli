84df9782eb8b961cefdf763f40a9753b3bd426c7
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-05-11 18:53:20 +0300

    fix: tillerless only if no tiller installed

--- plugins/helm/util/helm.sh
+++ plugins/helm/util/helm.sh
@@ -3,11 +3,15 @@
 # Runs tillerless Helm
 
 function finish {
-  helm tiller stop > /dev/null
+  if [[ ${taito_zone} != "gcloud-temp1" ]]; then
+    helm tiller stop > /dev/null
+  fi
 }
 trap finish EXIT
 
 ${taito_setv:?}
-helm tiller start-ci > /dev/null
-export HELM_HOST=127.0.0.1:44134
+if [[ ${taito_zone} != "gcloud-temp1" ]]; then
+  helm tiller start-ci > /dev/null
+  export HELM_HOST=127.0.0.1:44134
+fi
 helm "${@}"
