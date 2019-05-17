2c2e5fda79f8ff60b513fb92e01d9337f333074d
Temzasse <teemu.taskula@taitounited.fi>
2019-05-17 17:54:08 +0300

    feat: add algolia search

--- www/src/utils.js
+++ www/src/utils.js
@@ -1,6 +1,17 @@
 import { css } from '@emotion/core';
 import { BREAKPOINTS } from './constants';
 
+export const slugify = text => {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, ''); // Trim - from end of text
+};
+
 export const flattenListData = (data, key = 'allMarkdownRemark') => {
   return data[key].edges.map(
     ({ node: { fields = {}, frontmatter = {}, ...rest } }) => ({
