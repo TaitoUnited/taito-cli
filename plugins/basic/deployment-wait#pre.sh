5d82fbcab17273c98b4c1be94620c8cbfca2eb17
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-05-19 19:57:44 +0300

    feat(deployment): show running message

--- plugins/basic/deployment-wait#pre.sh
+++ plugins/basic/deployment-wait#pre.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 : "${taito_cli_path:?}"
 : "${taito_app_url:?}"
+: "${taito_target_env:?}"
 
 # Wait for deployment only if tests are executed after
 if [[ "${taito_mode:-}" == "ci" ]] && [[ "${ci_exec_test:-}" != "true" ]]; then
   echo "Skipping deployment wait because ci_exec_test != true"
+  echo
+  echo
+  echo "------------------------------------------------------"
+  echo "Your application is now starting up at ${taito_target_env}"
+  echo "------------------------------------------------------"
+  echo
+  echo
 else
   # Call next command on command chain
   "${taito_cli_path}/util/call-next.sh" "${@}"
+  echo
+  echo
+  echo "------------------------------------------------------"
+  echo "Your application is now running at ${taito_target_env}"
+  echo "------------------------------------------------------"
+  echo
+  echo
 fi
