10b3a736aa7cfac88b2f127761a1df3592839ca7
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-05-23 10:55:05 +0300

    chore: temporary backwards compatibility workarounds

--- plugins/helm/util/deploy.sh
+++ plugins/helm/util/deploy.sh
@@ -131,7 +131,12 @@ if [[ -d "./scripts/helm" ]]; then
     if [[ $exit_code != 0 ]] && [[ ${taito_zone} == "gcloud-temp1" ]] && \
        helm ls --namespace "${taito_namespace}" | grep "2017\|2018\|Jan\|Feb\|Mar\|Apr\|May"; then
       echo "Fix Helm chart after Kubernetes upgrade: delete chart and redeploy"
+      kubectl delete configmap "${taito_project}-common" \
+        --namespace "${taito_namespace}"
+      kubectl delete configmap "${taito_project}-${taito_target_env}-common" \
+        --namespace "${taito_namespace}"
       helm delete --purge "${taito_project}-${taito_target_env}"
+      sleep 15
       helm upgrade "${options[@]}" --debug --install \
         --namespace "${taito_namespace}" \
         --set global.env="${taito_target_env}" \
