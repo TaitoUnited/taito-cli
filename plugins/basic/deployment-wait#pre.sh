43e564dc736b66905788493249e21e4a29cbcd7a
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-07-15 11:56:13 +0300

    fix: add extra sleep before tests to make sure full rotate

--- plugins/basic/deployment-wait#pre.sh
+++ plugins/basic/deployment-wait#pre.sh
@@ -14,13 +14,17 @@ if [[ "${taito_mode:-}" == "ci" ]] && [[ "${ci_exec_test:-}" != "true" ]]; then
   echo
   echo
 else
+  extra_sleep_secs=${ci_test_wait:-35}
   # Call next command on command chain
   "${taito_util_path}/call-next.sh" "${@}"
   echo
   echo
   echo -e "${NOTEs}"
   echo "NOTE: Your application is now running at ${taito_target_env}!"
+  echo "Waiting extra ${extra_sleep_secs}s to make sure full container rotate."
+  echo "You can configure sleep seconds with the ci_test_wait environment variable."
   echo -e "${NOTEe}"
   echo
   echo
+  sleep "${extra_sleep_secs}"
 fi
