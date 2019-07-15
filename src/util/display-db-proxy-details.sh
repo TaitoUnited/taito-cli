d4f7e56b4c7f4da1d7fb832aa1fbcd9a0ca9d067
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-07-16 01:25:19 +0300

    fix(db-proxy): display only user accounts that exist

--- src/util/display-db-proxy-details.sh
+++ src/util/display-db-proxy-details.sh
@@ -8,7 +8,11 @@ echo "- database: ${database_name:-}"
 
 if [[ ${taito_mode:-} != "ci" ]]; then
   echo "- username and password:"
-  echo "  * Your personal username and password"
-  echo "  * ${database_mgr_username:-} / ${database_build_password:-}"
-  echo "  * ${database_app_username:-} / ${database_app_password:-}"
+  echo "  * Your personal database username and password (if you have one)"
+  if [[ "${database_mgr_username:-}" ]]; then
+    echo "  * ${database_mgr_username} / ${database_build_password:-}"
+  fi
+  if [[ "${database_app_username:-}" ]]; then
+    echo "  * ${database_app_username} / ${database_app_password:-}"
+  fi
 fi
