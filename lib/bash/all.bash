5a2bd741b721c15e3817c95cbf748b2dbf2119bd
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-07-18 12:08:15 +0300

    refactor: move libs to lib/

---
+++ lib/bash/all.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+: "${taito_lib_path:?}"
+
+# shellcheck source=docker.bash
+. "${taito_lib_path}/bash/docker.bash"
+# shellcheck source=exec.bash
+. "${taito_lib_path}/bash/exec.bash"
+# shellcheck source=host.bash
+. "${taito_lib_path}/bash/host.bash"
+# shellcheck source=host.bash
+. "${taito_lib_path}/bash/host_exports.bash"
+# shellcheck source=misc.bash
+. "${taito_lib_path}/bash/misc.bash"
+# shellcheck source=open.bash
+. "${taito_lib_path}/bash/open.bash"
+# shellcheck source=secret.bash
+. "${taito_lib_path}/bash/secret.bash"
+# shellcheck source=target.bash
+. "${taito_lib_path}/bash/target.bash"
+# shellcheck source=terminal.bash
+. "${taito_lib_path}/bash/terminal.bash"
