d8ad04365174199b7617df3d37b288e317d7df4d
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-11-04 10:36:11 +0200

    feat(kubectl): use db proxy instead of tcp proxy

--- plugins/kubectl/lib/proxy.bash
+++ plugins/kubectl/lib/proxy.bash
@@ -2,10 +2,19 @@
 
 function kubectl::db_proxy_start () {
   if [[ ${kubernetes_db_proxy_enabled:-} == "true" ]]; then
+    proxy_namespace="db-proxy"
+    proxy_instance="${database_instance:?}"
+
+    # tcp-proxy still in use in old test zones (TODO: REMOVE)
+    if [[ " gcloud-temp1 azure-test-256814 do-test-zone " == "${taito_zone:?}" ]]; then
+      proxy_namespace="tcp-proxy"
+      proxy_instance="tcp-proxy"
+    fi
+
     local proxy_pod
     proxy_pod=$(kubectl get pods --no-headers \
       --output=custom-columns=NAME:.metadata.name \
-      --namespace tcp-proxy | head -n 1
+      --namespace "${proxy_namespace}" | grep "${proxy_instance}" | head -n 1
     )
     echo "BIND ADDRESS: ${taito_db_proxy_bind_address:?}" > "${taito_vout:-}"
     taito::executing_start
@@ -13,7 +22,7 @@ function kubectl::db_proxy_start () {
       "${proxy_pod}" \
       "${database_port:?}:${database_real_port:-5432}" \
       --address "${taito_db_proxy_bind_address}" \
-      --namespace tcp-proxy > /dev/null &
+      --namespace "${proxy_namespace}" > /dev/null &
     sleep 1
     if [[ $1 != "true" ]]; then
       wait
