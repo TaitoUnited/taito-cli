1d6757d529219fc0da5a6e4d1887a5608605ba27
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-11-30 19:47:02 +0200

    Updates

---
+++ component---src-pages-plugins-js-277fd1110257e7c7c48d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{593:function(n,t,e){"use strict";e.r(t),e.d(t,"query",(function(){return d}));var r=e(2),u=(e(1),e(15)),a=e(588),c=e(587);t.default=function(n){var t=n.data,e=Object(u.c)(t,"md")[0];return Object(r.d)(a.a,null,Object(r.d)(c.a,null),Object(r.d)("div",{dangerouslySetInnerHTML:{__html:e.html}}))};var d="374385830"}}]);
+//# sourceMappingURL=component---src-pages-plugins-js-277fd1110257e7c7c48d.js.map
---
+++ styles-ec65a235d99de0a08007.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{375:function(n,o,w){},376:function(n,o,w){}}]);
+//# sourceMappingURL=styles-ec65a235d99de0a08007.js.map
