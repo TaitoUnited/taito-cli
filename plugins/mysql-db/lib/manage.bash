8c5d00d54141330e68a04add383c5132b6ba402f
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-12-17 22:11:12 +0200

    feat: retry db create/drop on wrong password

--- plugins/mysql-db/lib/manage.bash
+++ plugins/mysql-db/lib/manage.bash
@@ -2,9 +2,10 @@
 
 function mysql::create_database () {
   (
-    export MYSQL_PWD
-    MYSQL_PWD="${MYSQL_PWD}"
-    (
+    echo "Creating database"
+    # export MYSQL_PWD
+    # MYSQL_PWD="${MYSQL_PWD}"
+    until (
       taito::executing_start
       mysql -h "${database_host}" \
         -P "${database_port}" \
@@ -12,17 +13,22 @@ function mysql::create_database () {
         -u "${database_username}" \
         -e "set @database='${database_name}'; set @dbusermaster='${database_master_username:-root}'; set @dbusermgr='${database_name}'; set @dbuserapp='${database_name}a'; source ${taito_plugin_path}/resources/create.sql ;" \
         > "${taito_vout}"
-    )
+    ) do
+      :
+    done
 
     if [[ -f "./${taito_target:-database}/db.sql" ]]; then
-      (
+      echo "Initializing database"
+      until (
         taito::executing_start
         mysql -h "${database_host}" \
         -P "${database_port}" \
         -D "${database_name}" \
         -u "${database_username}" \
         -e "source ./${taito_target:-database}/db.sql ;" > "${taito_vout}"
-      )
+      ) do
+        :
+      done
     else
       echo "WARNING: File ./${taito_target:-database}/db.sql does not exist"
     fi
@@ -30,16 +36,22 @@ function mysql::create_database () {
 }
 
 function mysql::drop_database () {
-  taito::executing_start
-  mysql -h "${database_host}" \
-    -P "${database_port}" \
-    -D mysql \
-    -u "${database_username}" \
-    -e "set @database='${database_name}'; set @databaseold='${database_name}old'; source ${taito_plugin_path}/resources/drop.sql ;" \
-    > "${taito_vout}"
+  echo "Dropping database"
+  until (
+    taito::executing_start
+    mysql -h "${database_host}" \
+      -P "${database_port}" \
+      -D mysql \
+      -u "${database_username}" \
+      -e "set @database='${database_name}'; set @databaseold='${database_name}old'; source ${taito_plugin_path}/resources/drop.sql ;" \
+      > "${taito_vout}"
+  ) do
+    :
+  done
 }
 
 function mysql::create_users () {
+  echo "Creating users"
   taito::expose_db_user_credentials
 
   # Validate env variables
@@ -57,21 +69,30 @@ function mysql::create_users () {
 
   # Execute
 
-  taito::executing_start
-  mysql -h "${database_host}" \
-    -P "${database_port}" \
-    -D mysql \
-    -u "${database_username}" \
-    -e "set @database='${database_name}'; set @dbusermaster='${database_master_username:-root}'; set @dbusermgr='${database_name}'; set @dbuserapp='${database_name}a'; set @passwordapp='${database_app_password}'; set @passwordmgr='${database_build_password}'; source ${taito_plugin_path}/resources/users.sql ;" \
-    > "${taito_vout}" 2>&1
+  until (
+    taito::executing_start
+    mysql -h "${database_host}" \
+      -P "${database_port}" \
+      -D mysql \
+      -u "${database_username}" \
+      -e "set @database='${database_name}'; set @dbusermaster='${database_master_username:-root}'; set @dbusermgr='${database_name}'; set @dbuserapp='${database_name}a'; set @passwordapp='${database_app_password}'; set @passwordmgr='${database_build_password}'; source ${taito_plugin_path}/resources/users.sql ;" \
+      > "${taito_vout}" 2>&1
+  ) do
+    :
+  done
 }
 
 function mysql::drop_users () {
-  taito::executing_start
-  mysql -h "${database_host}" \
-    -P "${database_port}" \
-    -D mysql \
-    -u "${database_username}" \
-    -e "set @database='${database_name}'; set @dbusermaster='${database_master_username:-root}'; set @dbusermgr='${database_name}'; set @dbuserapp='${database_name}a'; source ${taito_plugin_path}/resources/drop-users.sql ;" \
-    > "${taito_vout}"
+  echo "Dropping users"
+  until (
+    taito::executing_start
+    mysql -h "${database_host}" \
+      -P "${database_port}" \
+      -D mysql \
+      -u "${database_username}" \
+      -e "set @database='${database_name}'; set @dbusermaster='${database_master_username:-root}'; set @dbusermgr='${database_name}'; set @dbuserapp='${database_name}a'; source ${taito_plugin_path}/resources/drop-users.sql ;" \
+      > "${taito_vout}"
+  ) do
+    :
+  done
 }
--- plugins/postgres-db/lib/manage.bash
+++ plugins/postgres-db/lib/manage.bash
@@ -2,10 +2,9 @@
 
 function postgres::create_database () {
   (
+    echo "Creating database"
     postgres::export_pgsslmode
-    export PGPASSWORD
-    PGPASSWORD="${PGPASSWORD}"
-    (
+    until (
       taito::executing_start
       psql -h "${database_host}" \
         -p "${database_port}" \
@@ -15,23 +14,30 @@ function postgres::create_database () {
         -v "database=${database_name}" \
         -v "dbusermaster=${database_master_username_internal:-postgres}" \
         -v "dbuserapp=${database_app_username_internal}" > "${taito_vout}"
-    )
+    ) do
+      :
+    done
 
     if [[ -f "./${taito_target:-database}/db.sql" ]]; then
-      (
+      echo "Initializing database"
+      until (
         taito::executing_start
         psql -h "${database_host}" \
           -p "${database_port}" \
           -d "${database_name}" \
           -U "${database_username}" \
           < "./${taito_target:-database}/db.sql" > "${taito_vout}"
-      )
+      ) do
+        :
+      done
     else
       echo "WARNING: File ./${taito_target:-database}/db.sql does not exist"
     fi
 
     taito::expose_db_user_credentials
 
+    echo "Granting user access"
+    export PGPASSWORD
     PGPASSWORD="${secret_value}"
     (
       taito::executing_start
@@ -48,18 +54,24 @@ function postgres::create_database () {
 }
 
 function postgres::drop_database () {
+  echo "Dropping database"
   postgres::export_pgsslmode
-  taito::executing_start
-  psql -h "${database_host}" \
-    -p "${database_port}" \
-    -d postgres \
-    -U "${database_username}" \
-    -f "${taito_plugin_path}/resources/drop.sql" \
-    -v "database=${database_name}" \
-    -v "databaseold=${database_name}_old" > "${taito_vout}" 2>&1
+  until (
+    taito::executing_start
+    psql -h "${database_host}" \
+      -p "${database_port}" \
+      -d postgres \
+      -U "${database_username}" \
+      -f "${taito_plugin_path}/resources/drop.sql" \
+      -v "database=${database_name}" \
+      -v "databaseold=${database_name}_old" > "${taito_vout}" 2>&1
+  ) do
+    :
+  done
 }
 
 function postgres::create_users () {
+  echo "Creating users"
   taito::expose_db_user_credentials
 
   # Validate env variables
@@ -76,28 +88,37 @@ function postgres::create_users () {
 
   # Execute
   postgres::export_pgsslmode
-  taito::executing_start
-  psql -h "${database_host}" -p "${database_port}" \
-    -d postgres \
-    -U "${database_username}" \
-    -f "${taito_plugin_path}/resources/users.sql" \
-    -v "database=${database_name}" \
-    -v "dbusermaster=${database_master_username_internal:-postgres}" \
-    -v "dbuserapp=${database_app_username_internal}" \
-    -v "passwordapp=${database_app_password}" \
-    -v "passwordbuild=${database_build_password}" \
-    > "${taito_vout}" 2>&1
+  until (
+    taito::executing_start
+    psql -h "${database_host}" -p "${database_port}" \
+      -d postgres \
+      -U "${database_username}" \
+      -f "${taito_plugin_path}/resources/users.sql" \
+      -v "database=${database_name}" \
+      -v "dbusermaster=${database_master_username_internal:-postgres}" \
+      -v "dbuserapp=${database_app_username_internal}" \
+      -v "passwordapp=${database_app_password}" \
+      -v "passwordbuild=${database_build_password}" \
+      > "${taito_vout}" 2>&1
+  ) do
+    :
+  done
 }
 
 function postgres::drop_users () {
+  echo "Dropping users"
   postgres::export_pgsslmode
-  taito::executing_start
-  psql -h "${database_host}" \
-    -p "${database_port}" \
-    -d postgres \
-    -U "${database_username}" \
-    -f "${taito_plugin_path}/resources/drop-users.sql" \
-    -v "database=${database_name}" \
-    -v "dbusermaster=${database_master_username_internal:-postgres}" \
-    -v "dbuserapp=${database_app_username_internal}" > "${taito_vout}"
+  until (
+    taito::executing_start
+    psql -h "${database_host}" \
+      -p "${database_port}" \
+      -d postgres \
+      -U "${database_username}" \
+      -f "${taito_plugin_path}/resources/drop-users.sql" \
+      -v "database=${database_name}" \
+      -v "dbusermaster=${database_master_username_internal:-postgres}" \
+      -v "dbuserapp=${database_app_username_internal}" > "${taito_vout}"
+  ) do
+    :
+  done
 }
