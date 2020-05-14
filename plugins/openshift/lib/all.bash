39ff9e05aeba83a7ff8b4b545108913eba9f8348
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2020-05-14 15:16:15 +0300

    feat(openshift): use kubernetes_name as the default openshift url

--- plugins/openshift/lib/all.bash
+++ plugins/openshift/lib/all.bash
@@ -2,18 +2,22 @@
 
 function openshift::authenticate () {
   local options=" ${*} "
+  local url="${openshift_url:-}"
+  if [[ ! ${url} ]] && [[ ${kubernetes_name:-} ]]; then
+    url="http://${kubernetes_name}"
+  fi
 
   if [[ ${options} == *" --reset "* ]] || \
-     ! oc status | grep "${openshift_url:?}" &> /dev/null;
+     ! oc status | grep "${url:?}" &> /dev/null;
   then
     if [[ ${openshift_certificate_file:-} ]]; then
-      oc login "${openshift_url:?}" \
+      oc login "${url:?}" \
         --certificate-authority="${openshift_certificate_file}"
     elif [[ ${openshift_token:-} ]]; then
-      oc login "${openshift_url:?}" \
+      oc login "${url:?}" \
         --token="${openshift_token}"
     else
-      oc login "${openshift_url:?}" \
+      oc login "${url:?}" \
         --username="${openshift_username:-}" \
         --password="${openshift_password:-}"
     fi
