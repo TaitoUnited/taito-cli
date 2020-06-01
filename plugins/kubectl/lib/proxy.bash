64583b579d262465d47e468c9dba321df963c91d
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2020-06-01 13:19:54 +0300

    feat: kubernetes container db proxy

--- plugins/kubectl/lib/proxy.bash
+++ plugins/kubectl/lib/proxy.bash
@@ -2,35 +2,55 @@
 
 function kubectl::db_proxy_start () {
   if [[ ${kubernetes_db_proxy_enabled:-} == "true" ]]; then
-    proxy_namespace="db-proxy"
-    proxy_instance="${database_instance:?}"
+    export taito_target=${taito_target:-database}
+    if taito::is_current_target_of_type container; then
+      (
+        kubectl::expose_pod_and_container false
+        echo "BIND ADDRESS: ${taito_db_proxy_bind_address:?}" > "${taito_vout:-}"
+        taito::executing_start
+        kubectl port-forward \
+          "${pod:?}" \
+          "${database_port:?}:${database_real_port:-5432}" \
+          --address "${taito_db_proxy_bind_address}" \
+          --namespace "${taito_namespace:?}" > /dev/null &
+        sleep 1
+        if [[ $1 != "true" ]]; then
+          wait
+        else
+          sleep 3
+        fi
+      )
+    else
+      proxy_namespace="db-proxy"
+      proxy_instance="${database_instance:?}"
 
-    # tcp-proxy still in use in old test zones (TODO: REMOVE)
-    if [[ " gcloud-temp1 azure-test-256814 do-test-zone " == "${taito_zone:-}" ]]; then
-      proxy_namespace="tcp-proxy"
-      proxy_instance="tcp-proxy"
-    fi
-
-    local proxy_pod
-    proxy_pod=$(kubectl get pods --no-headers \
-      --output=custom-columns=NAME:.metadata.name \
-      --namespace "${proxy_namespace}" | grep "${proxy_instance}" | head -n 1
-    )
-    echo "BIND ADDRESS: ${taito_db_proxy_bind_address:?}" > "${taito_vout:-}"
-    (
-      taito::executing_start
-      kubectl port-forward \
-        "${proxy_pod}" \
-        "${database_port:?}:${database_real_port:-5432}" \
-        --address "${taito_db_proxy_bind_address}" \
-        --namespace "${proxy_namespace}" > /dev/null &
-      sleep 1
-      if [[ $1 != "true" ]]; then
-        wait
-      else
-        sleep 3
+      # tcp-proxy still in use in old test zones (TODO: REMOVE)
+      if [[ " gcloud-temp1 azure-test-256814 do-test-zone " == "${taito_zone:-}" ]]; then
+        proxy_namespace="tcp-proxy"
+        proxy_instance="tcp-proxy"
       fi
-    )
+
+      local proxy_pod
+      proxy_pod=$(kubectl get pods --no-headers \
+        --output=custom-columns=NAME:.metadata.name \
+        --namespace "${proxy_namespace}" | grep "${proxy_instance}" | head -n 1
+      )
+      (
+        echo "BIND ADDRESS: ${taito_db_proxy_bind_address:?}" > "${taito_vout:-}"
+        taito::executing_start
+        kubectl port-forward \
+          "${proxy_pod}" \
+          "${database_port:?}:${database_real_port:-5432}" \
+          --address "${taito_db_proxy_bind_address}" \
+          --namespace "${proxy_namespace}" > /dev/null &
+        sleep 1
+        if [[ $1 != "true" ]]; then
+          wait
+        else
+          sleep 3
+        fi
+      )
+    fi
   fi
 }
 
