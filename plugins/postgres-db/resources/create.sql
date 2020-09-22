8dcf9f232f160021fd098af6880df8afc194aab0
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2020-09-22 10:09:21 +0300

    feat(postgres-db): add default revokes

--- plugins/postgres-db/resources/create.sql
+++ plugins/postgres-db/resources/create.sql
@@ -2,6 +2,8 @@
 
 CREATE DATABASE :database;
 
+REVOKE CONNECT ON DATABASE :database FROM PUBLIC;
+
 GRANT ALL PRIVILEGES ON DATABASE :database TO
   :dbusermaster, :database;
 GRANT CONNECT, TEMPORARY ON DATABASE :database TO
