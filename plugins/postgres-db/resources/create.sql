d86c9234668907d4bd65be298bc57397dfda441e
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2020-09-25 14:47:36 +0300

    feat(postgres-db): hide schema from public role

--- plugins/postgres-db/resources/create.sql
+++ plugins/postgres-db/resources/create.sql
@@ -2,9 +2,22 @@
 
 CREATE DATABASE :database;
 
-REVOKE CONNECT ON DATABASE :database FROM PUBLIC;
-
 GRANT ALL PRIVILEGES ON DATABASE :database TO
   :dbusermaster, :database;
 GRANT CONNECT, TEMPORARY ON DATABASE :database TO
   :dbuserapp, :dbuserviewer;
+
+-- Revoke connect permission from PUBLIC role
+REVOKE CONNECT ON DATABASE :database FROM PUBLIC;
+
+\connect :database
+
+-- Revoke public schema from PUBLIC
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON ALL TABLES IN SCHEMA public FROM PUBLIC;
+REVOKE ALL ON ALL SEQUENCES IN SCHEMA public FROM PUBLIC;
+REVOKE ALL ON ALL FUNCTIONS IN SCHEMA public FROM PUBLIC;
+
+-- Allow public schema for specific users
+GRANT USAGE, CREATE ON SCHEMA public TO :dbusermaster, :database;
+GRANT USAGE ON SCHEMA public TO :dbuserapp, :dbuserviewer;
