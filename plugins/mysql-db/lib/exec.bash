19ecab4ea12b61d770cfb313e0371c487a3134a5
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2020-09-29 18:15:55 +0300

    fix(mysql-db): ssl-mode not supported by current version of MariaDB client

--- plugins/mysql-db/lib/exec.bash
+++ plugins/mysql-db/lib/exec.bash
@@ -11,7 +11,9 @@ function mysql::print_ssl_options () {
     echo ""
   else
     taito::expose_db_ssl_credentials
-    echo -n "--ssl-mode=REQUIRED "
+    # NOTE: ssl-mode not supported by current version of MariaDB client:
+    # https://github.com/volatiletech/sqlboiler/issues/273
+    # echo -n "--ssl-mode=REQUIRED "
     echo -n "--ssl-ca=${database_ssl_ca_path} "
     echo -n "--ssl-cert=${database_ssl_cert_path} "
     echo -n "--ssl-key=${database_ssl_key_path}"
