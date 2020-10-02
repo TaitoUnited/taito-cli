be2cef4f81addd6a35e49d95c9b3debd6f9e438c
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2020-10-03 01:54:31 +0300

    Updates

---
+++ component---src-pages-plugins-js-57b83e82f807c3ff48f6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{591:function(n,t,e){"use strict";e.r(t),e.d(t,"query",(function(){return o}));var a=e(2),d=(e(1),e(15)),u=e(588),c=e(587),r=e(589),l=e(50);t.default=function(n){var t=n.data,e=Object(d.c)(t,"md")[0];return Object(a.d)(u.a,null,Object(a.d)(c.a,null),Object(a.d)("div",{dangerouslySetInnerHTML:{__html:e.html}}),Object(a.d)(l.a,{dir:"y",amount:20}),Object(a.d)(r.a,null))};var o="374385830"}}]);
+//# sourceMappingURL=component---src-pages-plugins-js-57b83e82f807c3ff48f6.js.map
---
+++ styles-0217377836c06380faff.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{375:function(n,o,w){},376:function(n,o,w){}}]);
+//# sourceMappingURL=styles-0217377836c06380faff.js.map
