2917eeffbb4427c5375b97c50325fca5c818f172
keskiju <jukka.keskiluopa@taitounited.fi>
2020-12-17 22:39:51 +0200

    feat: taito_default_browser

--- lib/bash/open.bash
+++ lib/bash/open.bash
@@ -10,31 +10,33 @@ function taito::convert_link () {
 }
 export -f taito::convert_link
 
-function taito::open_browser () {
+function taito::print_browser_command () {
   local url=$(taito::convert_link "${1:?}")
 
-  if [[ ${taito_host_os:-} == "windows" ]]; then
-    taito::execute_on_host "start chrome '${url}'"
-  elif [[ ${taito_host_os} == "macos" ]]; then
-    taito::execute_on_host "open -a 'Google Chrome' '${url}'"
-  else
-    taito::execute_on_host "xdg-open '${url}'"
+  if [[ $url == "https://"* ]] || [[ $url == "http://"* ]]; then
+    if [[ ${taito_host_os:-} == "windows" ]]; then
+      echo "start '${taito_default_browser:-chrome}' '${url}'"
+    elif [[ ${taito_host_os} == "macos" ]]; then
+      if [[ $taito_default_browser == "chrome" ]]; then
+        echo "open -a 'Google Chrome' '${url}'"
+      elif [[ $taito_default_browser ]]; then
+        echo "open -a '${taito_default_browser}' '${url}'"
+      else
+        echo "open '${url}'"
+      fi
+    else
+      echo "${taito_default_browser:-xdg-open} '${url}'"
+    fi
   fi
+}
+export -f taito::print_browser_command
+
+function taito::open_browser () {
+  taito::execute_on_host "$(taito::print_browser_command $1)"
 }
 export -f taito::open_browser
 
 function taito::open_browser_fg () {
-  local url=$(taito::convert_link "${1:?}")
-
-  if [[ ${taito_host_os:-} == "windows" ]]; then
-    taito::execute_on_host_fg \
-      "start chrome '${url}'"
-  elif [[ ${taito_host_os} == "macos" ]]; then
-    taito::execute_on_host_fg \
-      "open -a 'Google Chrome' '${url}'"
-  else
-    taito::execute_on_host_fg \
-      "xdg-open '${url}'"
-  fi
+  taito::execute_on_host_fg "$(taito::print_browser_command $1)"
 }
 export -f taito::open_browser_fg
