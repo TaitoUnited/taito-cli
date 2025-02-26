9b1e09c631841851b7acd15d001ae023a531083d
keskiju <jukka.keskiluopa@taitounited.fi>
2025-02-26 11:02:12 +0200

    feat(postgres-db): grant pg_signal_backend for mgr user

--- plugins/postgres-db/resources/create-users.sql
+++ plugins/postgres-db/resources/create-users.sql
@@ -10,3 +10,6 @@ ALTER USER :dbuserapp WITH PASSWORD :qpasswordapp;
 CREATE USER :dbusermgr PASSWORD :qpasswordbuild
   NOSUPERUSER CREATEDB NOCREATEROLE CONNECTION LIMIT 20;
 ALTER USER :dbusermgr WITH PASSWORD :qpasswordbuild;
+
+-- TODO: only needed when mgr account is used instead of developers personal account
+GRANT pg_signal_backend TO :dbusermgr;
