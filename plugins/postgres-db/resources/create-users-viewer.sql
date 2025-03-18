444733341ce3735552fdc9ad8ee847907e5a5dbc
keskiju <jukka.keskiluopa@taitounited.fi>
2025-03-19 00:22:17 +0200

    feat(postgres): set detault statement_timeout for users

--- plugins/postgres-db/resources/create-users-viewer.sql
+++ plugins/postgres-db/resources/create-users-viewer.sql
@@ -5,3 +5,4 @@
 CREATE USER :dbuserviewer PASSWORD :qpasswordviewer
   NOSUPERUSER NOCREATEDB NOCREATEROLE CONNECTION LIMIT 20;
 ALTER USER :dbuserviewer WITH PASSWORD :qpasswordviewer;
+ALTER ROLE :dbuserviewer SET statement_timeout = '300s';
--- plugins/postgres-db/resources/create-users.sql
+++ plugins/postgres-db/resources/create-users.sql
@@ -6,10 +6,12 @@
 CREATE USER :dbuserapp PASSWORD :qpasswordapp
   NOSUPERUSER NOCREATEDB NOCREATEROLE CONNECTION LIMIT 40;
 ALTER USER :dbuserapp WITH PASSWORD :qpasswordapp;
+ALTER ROLE :dbuserapp SET statement_timeout = '300s';
 
 CREATE USER :dbusermgr PASSWORD :qpasswordbuild
   NOSUPERUSER CREATEDB NOCREATEROLE CONNECTION LIMIT 20;
 ALTER USER :dbusermgr WITH PASSWORD :qpasswordbuild;
+ALTER ROLE :dbusermgr SET statement_timeout = '600s';
 
 -- TODO: only needed when mgr account is used instead of developers personal account
 GRANT pg_signal_backend TO :dbusermgr;
